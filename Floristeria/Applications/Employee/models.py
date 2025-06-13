e1c15ff9f111d8358cf3a0dc0f51bffa8698fc16
Mind Brand Windows <148798600+mindbrand729@users.noreply.github.com>
2025-06-12 21:09:20 -0500

    Correccion del modelo Employee (Trabajador)

--- Floristeria/Applications/Employee/models.py
+++ Floristeria/Applications/Employee/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    name_employee = models.CharField(max_length=100)
-    surname_employee = models.CharField(max_length=100)
-    mail_employee = models.EmailField()
-    card_employee = models.CharField(max_length=20, unique=True)
-    code_employee = models.CharField(max_length=20, unique=True)
+    name_employee = models.CharField(max_length=100, blank=False, null=False)
+    surname_employee = models.CharField(max_length=100, blank=False, null=False)
+    mail_employee = models.EmailField(blank=False, null=False)
+    card_employee = models.CharField(max_length=20, unique=True, blank=False, null=False)
+    code_employee = models.CharField(max_length=20, unique=True, blank=False, null=False)
     imagen_employee = models.ImageField(upload_to='trabajadores/', blank=True, null=True)
 
     def __str__(self):
