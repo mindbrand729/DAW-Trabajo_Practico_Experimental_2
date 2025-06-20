1a93f3ecb7abc9671e777a63aaf5c00bcbe5bbf8
Mind Brand Windows <148798600+mindbrand729@users.noreply.github.com>
2025-06-19 22:42:41 -0500

    cambio de archivos de migracion

--- Floristeria/Applications/Company/models.py
+++ Floristeria/Applications/Company/models.py
@@ -8,4 +8,4 @@ class Company(models.Model):
     vision_company = models.TextField(blank=False, null=False)
     founding_company = models.PositiveIntegerField(blank=False, null=False)
     ruc_company = models.CharField(max_length=13, unique=True, blank=False, null=False)
-    imagen_company = models.ImageField(upload_to='empresas/', blank=True, null=True)
+    imagen_company = models.ImageField(upload_to='company/', blank=True, null=True)
