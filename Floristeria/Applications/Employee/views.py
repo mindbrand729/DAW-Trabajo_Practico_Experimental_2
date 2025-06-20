b3217cf7d8f0cedcd64010cfc9e93ef7e59aa987
walarconv <walarconv@unemi.edu.ec>
2025-06-20 16:16:23 -0500

    Ingresando arrivos para trabajadores y se hizo cambio en base.html se agrego href

--- Floristeria/Applications/Employee/views.py
+++ Floristeria/Applications/Employee/views.py
@@ -7,26 +7,34 @@ from .models import Employee
 class EmployeeListView(ListView):
     model = Employee
     template_name = 'employee_list.html'
-    context_object_name = 'employee'
+    context_object_name = 'employees'
 
-class EmployeeDetailView(DetailView):
+class EmployeeDetailView(ListView):
     model = Employee
     template_name = 'employee_detail.html'
-    context_object_name = 'employee'
+    context_object_name = 'employees'
+
+    def get_queryset(self):
+        Idemployee = 4
+        return Employee.objects.filter(id=Idemployee)
+
 
 class EmployeeCreateView(CreateView):
     model = Employee
     template_name = 'employee_form.html'
     fields = '__all__'
-    success_url = reverse_lazy('employee_list')
+    success_url = '/employee'
 
 class EmployeeUpdateView(UpdateView):
     model = Employee
-    template_name = 'employee_form.html'
+    template_name = 'employee_formup.html'
     fields = '__all__'
-    success_url = reverse_lazy('employee_list')
+    context_object_name = 'Upemployee'
+    success_url = '/employee'
+    
 
 class EmployeeDeleteView(DeleteView):
     model = Employee
     template_name = 'employee_delete.html'
-    success_url = reverse_lazy('employee_list')   
+    success_url = '/employee'
+    context_object_name = 'delemployee'  
