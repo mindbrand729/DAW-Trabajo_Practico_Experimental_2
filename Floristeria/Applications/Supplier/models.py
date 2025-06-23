03a2f092c7cf3957f0d08b759489cbc578a522cc
al802 <yourname@example.com>
2025-06-23 11:01:37 -0500

    Modificar

--- Floristeria/Applications/Supplier/models.py
+++ Floristeria/Applications/Supplier/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 class Supplier(models.Model):
     name_supplier = models.CharField(max_length=200, blank=False, null=False, verbose_name="Nombre del Proveedor")
-    description_supplier = models.TextField(blank=False, null=False, verbose_name="Descripción")
     phone_supplier = models.CharField(max_length=20, blank=False, null=False, verbose_name="Teléfono")
     country_supplier = models.CharField(max_length=100, blank=False, null=False, verbose_name="País")
     mail_supplier = models.EmailField(blank=False, null=False, verbose_name="Correo Electrónico")
-    address_supplier = models.TextField(blank=False, null=False, verbose_name="Dirección")
+
